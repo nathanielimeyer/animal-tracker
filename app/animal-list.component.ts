2dfeea234bf1683751e5fd5b096bf82fa8a96e26
Nathaniel I Meyer <nathanielimeyer@gmail.com>
2017-05-05 10:17:56 -0700

    Add list component and selector and an array of animals

---
+++ app/animal-list.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Animal } from './animal.model';
+
+@Component({
+  selector: 'animal-list',
+  template: `
+    <ul>
+      <li *ngFor="let currentAnimal of childAnimalList">{{currentAnimal.name}} the {{currentAnimal.species}}</li>
+    </ul>
+  `
+})
+
+export class AnimalListComponent {
+  @Input() childAnimalList: Animal[];
+
+}
--- app/app.component.ts
+++ app/app.component.ts
@@ -5,9 +5,14 @@ import { Animal } from './animal.model';
   selector: 'app-root',
   template: `
   <h1>Animal Tracker</h1>
+  <animal-list [childAnimalList]="masterAnimalList"></animal-list>
   `
 })
 
 export class AppComponent {
-
+  masterAnimalList: Animal[] = [
+    new Animal('Arctic Fox', 'Moon', 2, 'Carnivore', 'Northern Trail', 5, 'Female', 'Cool shade', 'Loud noises'),
+    new Animal('Ocelot', 'Prince', 4, 'Carnivore', 'Tropical Rain Forest Building', 6, 'Male', 'Laying in the sunshine', 'Toys that are not rope-based'),
+    new Animal('Northwest Black Tailed Deer', 'Tinkerbell', 8, 'Herbivore', 'Northern Trail', 2, 'Female', 'Delicate roots and leaves', 'Loud Noises')
+  ]
 }
